34065660487c65493c1c0b205dcae772e4d35e59
Shin, SJ <lannex.shin@gmail.com>
2022-10-26 20:22:24 +0900

    chore: nest 설정

--- apps/www/src/app/app.module.ts
+++ apps/www/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { ConfigModule } from '../environments/config.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
