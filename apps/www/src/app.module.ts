2614482d9e582251f5dc3240aede9d79e61f2fce
Shin, SJ <lannex.shin@gmail.com>
2022-10-27 18:31:10 +0900

    chore: app 초기 설정(validation, serializer)

--- apps/www/src/app.module.ts
+++ apps/www/src/app.module.ts
@@ -1,11 +1,35 @@
-import { Module } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Module,
+  ValidationPipe,
+} from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 
 import { ConfigModule } from './configs/config.module';
 import { OrmModule } from './configs/orm.module';
 
+const providers = [
+  {
+    provide: APP_PIPE,
+    useValue: new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  },
+  {
+    provide: APP_INTERCEPTOR,
+    useClass: ClassSerializerInterceptor,
+  },
+  // {
+  //   provide: APP_FILTER,
+  //   useClass: AllExceptionsFilter,
+  // },
+];
+
 @Module({
   imports: [ConfigModule, OrmModule],
   controllers: [],
-  providers: [],
+  providers,
 })
 export class AppModule {}
