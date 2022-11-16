f1ea064e94b644d2b7770b7c684b86105adcdbea
Shin, SJ <lannex.shin@gmail.com>
2022-11-16 22:54:03 +0900

    feat: shared-configs 모듈 추가

--- apps/www/src/configs/modules/index.ts
+++ apps/www/src/configs/modules/index.ts
@@ -1,7 +1,8 @@
 import { CqrsModule } from '@nestjs/cqrs';
 
+import { MapperModule } from '@nestjs-template/shared-configs';
+
 import { ConfigModule } from './config.module';
-import { MapperModule } from './mapper.module';
 import { OrmModule } from './orm.module';
 
 export default [ConfigModule, OrmModule, MapperModule, CqrsModule];
