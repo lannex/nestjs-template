bf15aa526e1d6ab4f610c0627639a8f5d830f99a
Shin, SJ <lannex.shin@gmail.com>
2022-11-22 12:05:30 +0900

    chore(config): 수정

--- apps/www/src/configs/values.ts
+++ apps/www/src/configs/values.ts
@@ -1,8 +1,14 @@
 import config from 'config';
+import { cleanEnv, str } from 'envalid';
 
-export const isProduction = process.env.NODE_ENV === 'production';
+const env = cleanEnv(process.env, {
+  APP_NAME: str(),
+  NODE_ENV: str({ choices: ['local', 'development', 'production'] }),
+});
 
-export const appName = 'www';
+export const isProduction = env.NODE_ENV === 'production';
+
+export const appName = env.APP_NAME;
 
 export const host = config.get<string>(`${appName}.host`);
 export const port = config.get<string>(`${appName}.port`);
